831a2c9133f4cbb8ad6f1314cde11875a70472b9
Achraf Moubarek <amoubare@e2r1p4.1337.ma>
2021-08-10 19:31:47 +0100

    1

---
+++ ex00/ft_ft.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+int main()
+{
+	int nbr;
+	nbr  = 5752875;
+	ft_ft(&nbr);
+	printf("%d", nbr);
+}
---
+++ ex02/ft_swap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void ft_swap(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c ;
+
+
+}
+
+int main()
+{
+	int	a;
+	int	b;
+   
+	a = 21;
+   	b = 20;
+	ft_swap(&a, &b);
+	printf("%d\n", a);
+	printf("%d", b);
+
+}
